ead77bda319d2d2051c5ae4ed23fbcf06888f8c3
Amr Said Abd Elrhim Elskaan <amrelskaan@hotmail.com>
2022-06-02 10:18:52 +0200

    update project

---
+++ BSChatBotClient.xcframework/ios-arm64_i386_x86_64-simulator/BSChatBotClient.framework/Headers/BSChatBotClient.h
@@ -0,0 +1,18 @@
+//
+//  BSChatBotClient.h
+//  BSChatBotClient
+//
+//  Created by Tarek Sabry on 23/02/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BSChatBotClient.
+FOUNDATION_EXPORT double BSChatBotClientVersionNumber;
+
+//! Project version string for BSChatBotClient.
+FOUNDATION_EXPORT const unsigned char BSChatBotClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BSChatBotClient/PublicHeader.h>
+
+
